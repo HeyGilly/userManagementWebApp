a0735d59869bb0c486bd0199ff47a94bdba20f52
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-11-29 02:35:35 -0600

    created a servlet

--- src/main/java/com/heygilly/usermanagementwebapp/web/UserServlet.java
+++ src/main/java/com/heygilly/usermanagementwebapp/web/UserServlet.java
@@ -1,6 +1,130 @@
 package com.heygilly.usermanagementwebapp.web;
 
-import javax.servlet.http.HttpServlet;
+import com.heygilly.usermanagementwebapp.dao.UserDAO;
+import com.heygilly.usermanagementwebapp.model.User;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "list layout", value = "/")
 public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+                case "/new":
+                    showNewForm(request, response);
+                    break;
+                case "/insert":
+                    insertUser(request, response);
+                    break;
+                case "/delete":
+                    deleteUser(request, response);
+                    break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
+                case "/update":
+                    updateUser(request, response);
+                    break;
+                default:
+                    listUser(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    //--SHOW THE LIST
+    private void listUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<User> listUser = userDAO.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    //show the form for new user
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    //show edit form
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User existingUser = userDAO.selectUser(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+        request.setAttribute("user", existingUser);
+        dispatcher.forward(request, response);
+    }
+
+    // insert user
+    private void insertUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        String first_name = request.getParameter("First Name");
+        String last_name = request.getParameter("Last Name");
+        String email = request.getParameter("Email");
+        String username = request.getParameter("Username");
+        String city = request.getParameter("City");
+        String state = request.getParameter("State");
+
+        User newUser = new User(first_name, last_name, email, username, city, state);
+        userDAO.insertUser(newUser);
+        response.sendRedirect("list");
+    }
+
+    //update user
+    private void updateUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String first_name = request.getParameter("First Name");
+        String last_name = request.getParameter("Last Name");
+        String email = request.getParameter("Email");
+        String username = request.getParameter("Username");
+        String city = request.getParameter("City");
+        String state = request.getParameter("State");
+
+        User updatePlayer = new User(id, first_name, last_name, email, username, city, state);
+        userDAO.updateUser(updatePlayer);
+        response.sendRedirect("list");
+    }
+
+    //delete user
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDAO.deleteUser(id);
+        response.sendRedirect("list");
+    }
+
+
+
+
+
+
 }
